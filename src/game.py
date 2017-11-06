aa796da7eaacd967894ca0189280a5723df3c168
Adrian <adrianc-a@users.noreply.github.com>
2017-11-05 17:38:54 -0800

    stubs on 11/7 meeting

---
+++ src/game.py
@@ -0,0 +1,27 @@
+class Tree():
+    # maintain edges, vertices etc.
+    pass
+
+class StateManager():
+
+    def __init__(self):
+        self.cur_state = None
+
+    def legal_moves():
+        pass
+
+class TicTacToe(StateManager):
+    pass
+
+class Chess(StateManager):
+    pass
+
+class GameTree():
+
+    # maintain game tree, specific to game
+    def __init__(self, state_manager, nn):
+        self.tree = None
+
+    # updates the tree
+    def mcts(self, simulations):
+        pass
