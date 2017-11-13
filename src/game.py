f600ffe4eba5d4501e6609236d12dc853c3ff708
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2017-11-13 15:28:56 -0800

    general idea

--- src/game.py
+++ src/game.py
@@ -1,3 +1,5 @@
+
+
 class Tree():
     # maintain edges, vertices etc.
     pass
@@ -25,9 +27,53 @@ class GameTree():
     # updates the tree
     def mcts(self, simulations):
         pass
-    
+    def play_move():
+        pass
+
+
     # feel free to change the names
     def backup(self):
         pass
     def forward(self):
         pass
+def train():
+    pass
+
+
+
+
+
+## ideal flow
+
+player = create_empty_player_ttt()
+player.train(num_iters=20)
+
+
+
+class Game():
+    pass
+
+class Game(GamePlayer):
+    pass
+
+
+
+
+
+## ideal flow
+
+player = create_empty_player_ttt()
+player.train(num_iters=20)
+
+
+
+class Game():
+    pass
+
+class Game(GamePlayer):
+    pass
+
+gp = TTTGame(player, HumanPlayer())
+gp.play()
+
+
