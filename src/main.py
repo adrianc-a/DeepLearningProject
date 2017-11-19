6cec96738c8cb2da091e6d03cc3d0da1b8607a04
Adrian Chmielewski-Anders <adrianc-a@users.noreply.github.com>
2017-11-18 17:17:09 -0800

    adding human player, alphago stubs

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ import players
 from game import Game
 from tictactoe_manager import TicTacToeManager
 
-g = Game(TicTacToeManager(), players.ttt_simple_player, players.ttt_random_player)
+g = Game(TicTacToeManager(), players.ttt_human_player, players.ttt_random_player)
 g.play()
 
--- src/players.py
+++ src/players.py
@@ -7,4 +7,14 @@ def ttt_simple_player(state, moves):
 def ttt_random_player(state, moves):
     return randint(0, len(moves) - 1)
 
+def ttt_human_player(state, moves):
+    inp = input('Your move: r c:\n')
+    row = int(inp[0])
+    col = int(inp[2])
+    move = 3 * row + col
 
+    for m in range(len(moves)):
+        board = moves[m].board.board
+        if board[move] != '.':
+            return m
+    return 0
