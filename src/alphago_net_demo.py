1a81eccd76acc6eda4896453ab2b85051248c9c3
Kenan Nalbant <kanalbant@gmail.com>
2017-11-24 16:07:33 -0800

    Updated alphago_net_demo to reflect moves2vec change
    Update examples in alphago_net_demo to reflect new way
    to interact with state manager using moves2vec

--- src/alphago_net_demo.py
+++ src/alphago_net_demo.py
@@ -1,4 +1,4 @@
-import chess_manager as cm
+input_batchimport chess_manager as cm
 from importlib import reload
 import chess
 import timeit
@@ -8,19 +8,19 @@ from keras import backend as K
 from keras.layers import Dense, Activation,Conv2D,MaxPooling2D,Flatten,Dropout, BatchNormalization
 import networks as nn
 reload(nn)
+reload(cm)
 
 sess = tf.Session()
 K.set_session(sess)
 
 g = cm.ChessManager()
-vec = g.state2vec()
 
 # create a 'batch' of the input data
-new_vec = np.concatenate((vec,vec,vec))
+input_batch = g.moves2vec()
 
 # create a batch of 'label' data
-valy = np.random.uniform(low=-1,high=1,size=(3,1))
-poly = np.random.uniform(low=0,high=1,size=(3,1))
+valy = np.random.uniform(low=-1,high=1,size=(input_batch.shape[0],1))
+poly = np.random.uniform(low=0,high=1,size=(input_batch.shape[0],1))
 
 (inp,valY,polY,pl_out,v_out,loss) = nn.alphago_net((3,8,8), 256, (3,3), 1, 256, (3,3))
 
@@ -34,12 +34,12 @@ sess.run(init_op)
 # get the predicted output for policy and reward
 with sess.as_default():
         # this would be the line to run during generating the tree
-        net_out = sess.run((v_out, pl_out), feed_dict={inp:new_vec, K.learning_phase(): 0})
+        net_out = sess.run((v_out, pl_out), feed_dict={inp:input_batch, K.learning_phase(): 0})
 
 # calculate the actual value of the loss function
 with sess.as_default():
         # this gives you the actual loss
-        loss_out = sess.run(loss, feed_dict={inp:new_vec, valY:valy,
+        loss_out = sess.run(loss, feed_dict={inp:input_batch, valY:valy,
                                              polY:poly, K.learning_phase(): 0})
 
 k_old = sess.run(tf.trainable_variables()[0])
@@ -47,7 +47,7 @@ k_old = sess.run(tf.trainable_variables()[0])
 with sess.as_default():
 	batch = vec
 	for i in range(100):
-            train_step.run(feed_dict={inp: new_vec, valY:valy, polY:poly, K.learning_phase(): 1})
+            train_step.run(feed_dict={inp: input_batch, valY:valy, polY:poly, K.learning_phase(): 1})
             k = sess.run(tf.trainable_variables()[0])
 
 
@@ -62,11 +62,11 @@ optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
 net = nn.NetworkWrapper(network, optimizer)
 
 # get the predicted pi and z values for a batch of data
-policy_out, value_out = net.forward(new_vec)
+policy_out, value_out = net.forward(input_batch)
 
 # get the value of the loss function for a batch of data
-loss_val = net.forward_loss(new_vec, poly, valy)
+loss_val = net.forward_loss(input_batch, poly, valy)
 
 # running 100 training steps given a batch
 for i in range(100):
-    net.training_step(new_vec, poly, valy)
+    net.training_step(input_batch, poly, valy)
