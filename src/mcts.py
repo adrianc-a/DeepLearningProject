c9a91d0807e6c7a3313254d401dd69d994cd4a0f
Parsoa <parsoakhorsand91@gmail.com>
2017-11-25 19:07:41 -0800

    Modify pi and p value usage in MCTS

--- src/mcts.py
+++ src/mcts.py
@@ -13,20 +13,19 @@ class Node(object):
         self.moves = state_manager.get_moves()
         # the action parent took to arrive here
         self.move_in = move_in
-        # initally all moves are untried
-        self.untried_actions = list(map(lambda x: x, self.moves))
         # tried moves
         self.children = []
-        # 
-        self.v = 0
+        #
+        self.value = 0
+        #
         self.n = {}
+        self.p = {}
+        self.v = {}
         self.q = {}
         self.u = {}
-        self.p = {}
-        self.visit_count = 0
 
-    def has_untried_actions(self):
-        return not len(self.untried_actions) == 0
+    def is_leaf(self):
+        return len(self.children) == 0
 
     def is_terminal(self):
         return self.state_manager.is_terminal_state()
@@ -45,24 +44,30 @@ class Node(object):
         best_child = random.choice(candidates)
         return best_child
 
-    def expand(self):
-        move = self.untried_actions[random.choice(range(0, len(self.untried_actions)))]
-        child = Node(state_manager = self.state_manager.make_move(self.moves.index(move)),
-            parent = self, move_in = move)
-        self.n[child] = 0.0
-        self.children.append(child)
-        return child
+    def expand(self, network_wrapper):
+        self.value = 0.0
+        for move in self.moves:
+            child = Node(state_manager = self.state_manager.make_move(self.moves.index(move)),
+                parent = self, move_in = move)
+            (p, v) = network_wrapper.forward(np.array[child.state_manager])
+            self.v[child] = v
+            self.p[child] = p
+            self.n[child] = 0.0
+            self.children.append(child)
+        return self
 
-    def update(self, child, v, move_number):
-        self.visit_count += 1
-        self.v += v
+    def update(self, child, v):
+        self.value += v
         self.n[child] += 1.0
         # maybe add 1 to both nominator and denominator to avoid zero?
         for child in self.children:
-            self.p[child] = self.n[child] / (self.visit_count)
-            self.q[child] = (1 / self.n[child]) * self.v
-            c_puct = (1.0 if move_number < 30 else 0.05)
-            self.u[child] = c_puct * self.p[child] * ((self.visit_count ** 0.5) / (1 + self.n[child]))
+            self.q[child] = 0 if self.n[child] == 0 else (1 / self.n[child]) * self.value
+            self.u[child] = self.p[child] * ((sum(list(map(lambda x: self.n[x], self.children))) ** 0.5) / (1 + self.n[child]))
+
+    def export_pi(self, move_number):
+        temperature = (1.0 if move_number < 30 else 0.05) 
+        s = sum(list(map(lambda x: self.n[x], self.children)))
+        return list(map(lambda x: self.n[x] ** (1.0 / temperature)))
 
 # ============================================================================================================================ #
 # MCTS
@@ -70,29 +75,31 @@ class Node(object):
 
 class MCTS(object):
 
-    def __init__(self, tree_policy):
+    def __init__(self, tree_policy, network_wrapper):
         self.tree_policy = tree_policy
+        self.network_wrapper = network_wrapper
 
     def __call__(self, state_manager, move_number, n = 1500):
         root = Node(state_manager = state_manager, parent = None, move_in = None)
         for i in range(n):
-            node = self.get_next_node(root)
-            self.back_propagate(node, self.evaluate_nn(node), move_number)
+            node = self.traverse(root)
+            self.back_propagate(node)
         # now that the simulation is done, return some node
-        return root.get_best_child(self.tree_policy).move_in
+        return root.export_pi(move_number)
 
-    def get_next_node(self, node):
+    def traverse(self, node):
         # either expand 
         while not node.is_terminal():
-            if node.has_untried_actions:
-                return node.expand()
+            if node.is_leaf():
+                return node.expand(self.network_wrapper)
             else:
                 node = node.get_best_child(self.tree_policy)
         return node
 
-    def back_propagate(self, node, v, move_number):
+    def back_propagate(self, node):
+        v = node.parent.v[node] if node.parent else 0
         while node.parent is not None:
-            node.parent.update(child = node, v = v, move_number = move_number)
+            node.parent.update(child = node, v = v)
             node = node.parent
 
     def evaluate_nn(self, node):
